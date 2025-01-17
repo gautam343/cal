7a6f740df341d736831041181c50f935907ae244
Vineet <vineetsah1407@gmail.com>
2025-01-17 15:28:23 +0530

    change

--- log.py
+++ log.py
@@ -4,13 +4,3 @@ import math
 def log(x,y =math.e):
     return math.log(x,y)
  
-# Get input from the user
-number = float(input("Enter the number: "))
-base = input("Enter the base (default is e): ")
-
-# Convert base input to float if provided, otherwise use default
-base = float(base) if base else math.e
-
-# Call the log function and print the result
-result = log(number, base)
-print(f"The logarithm of {number} with base {base} is {result}")
